cacbd6cd63f1288853ae020eefbd8679513d06c1
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-07-28 21:31:53 -0300

    hdl: Add interrupt concat for Fabric Interrupts

--- hdl/scripts/bd.tcl
+++ hdl/scripts/bd.tcl
@@ -11,10 +11,10 @@ namespace eval ::fsat_bd {
         set parentObj [get_bd_cells $parentCell]
         current_bd_instance $parentObj
 
-        set DDR [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 DDR]
-        set FIXED_IO [create_bd_intf_port -mode Master -vlnv xilinx.com:display_processing_system7:fixedio_rtl:1.0 FIXED_IO]
-        set IIC_0 [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:iic_rtl:1.0 IIC_0]
-        set IIC_1 [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:iic_rtl:1.0 IIC_1]
+        set ddr [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 ddr]
+        set fixed_io [create_bd_intf_port -mode Master -vlnv xilinx.com:display_processing_system7:fixedio_rtl:1.0 fixed_io]
+        set iic_0 [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:iic_rtl:1.0 iic_0]
+        set iic_1 [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:iic_rtl:1.0 iic_1]
 
         set zynq_ps [create_zynq_ps]
 
@@ -23,14 +23,35 @@ namespace eval ::fsat_bd {
         set xlconstant_0 [create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0]
         set_property CONFIG.CONST_VAL {0} $xlconstant_0
 
-        connect_bd_intf_net -intf_net zynq_ps_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins zynq_ps/DDR]
-        connect_bd_intf_net -intf_net zynq_ps_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins zynq_ps/FIXED_IO]
-        connect_bd_intf_net -intf_net zynq_ps_IIC_0 [get_bd_intf_ports IIC_0] [get_bd_intf_pins zynq_ps/IIC_0]
-        connect_bd_intf_net -intf_net zynq_ps_IIC_1 [get_bd_intf_ports IIC_1] [get_bd_intf_pins zynq_ps/IIC_1]
+        set irq_concat [create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 irq_concat]
+        set_property -dict [list CONFIG.NUM_PORTS {16}] $irq_concat
 
-        connect_bd_net -net zynq_ps_FCLK_CLK0 [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins proc_sys_reset_0/slowest_sync_clk]
-        connect_bd_net -net zynq_ps_FCLK_RESET0_N [get_bd_pins zynq_ps/FCLK_RESET0_N] [get_bd_pins proc_sys_reset_0/ext_reset_in]
+        connect_bd_intf_net -intf_net zynq_ps_ddr [get_bd_intf_ports ddr] [get_bd_intf_pins zynq_ps/DDR]
+        connect_bd_intf_net -intf_net zynq_ps_fixed_io [get_bd_intf_ports fixed_io] [get_bd_intf_pins zynq_ps/FIXED_IO]
+        connect_bd_intf_net -intf_net zynq_ps_iic_0 [get_bd_intf_ports iic_0] [get_bd_intf_pins zynq_ps/IIC_0]
+        connect_bd_intf_net -intf_net zynq_ps_iic_1 [get_bd_intf_ports iic_1] [get_bd_intf_pins zynq_ps/IIC_1]
+
+        connect_bd_net -net zynq_ps_fclk_clk0 [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins proc_sys_reset_0/slowest_sync_clk]
+        connect_bd_net -net zynq_ps_fclk_reset0_n [get_bd_pins zynq_ps/FCLK_RESET0_N] [get_bd_pins proc_sys_reset_0/ext_reset_in]
         connect_bd_net -net xlconstant_0_dout [get_bd_pins xlconstant_0/dout] [get_bd_pins zynq_ps/SDIO0_WP]
+
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In0]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In1]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In2]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In3]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In4]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In5]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In6]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In7]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In8]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In9]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In10]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In11]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In12]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In13]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In14]
+        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In15]
+        connect_bd_net [get_bd_pins irq_concat/dout] [get_bd_pins zynq_ps/IRQ_F2P]
 
         # Set PFM properties
         set_property SYNTH_CHECKPOINT_MODE "Hierarchical" [get_files [current_bd_design].bd] 
