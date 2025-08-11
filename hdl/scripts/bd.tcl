6b96128993c8d129146a346c31c83883fc361334
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-08-11 20:52:36 -0300

    hdl: Add axi_iic for open-drain translator and fix spi ports

--- hdl/scripts/bd.tcl
+++ hdl/scripts/bd.tcl
@@ -14,6 +14,7 @@ namespace eval ::fsat_bd {
         set ddr [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 ddr]
         set fixed_io [create_bd_intf_port -mode Master -vlnv xilinx.com:display_processing_system7:fixedio_rtl:1.0 fixed_io]
         set iic_sens [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:iic_rtl:1.0 iic_sens]
+        set iic_0 [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:iic_rtl:1.0 iic_0]
         set zynq_ps [create_zynq_ps]
 
         # Main processor reset
@@ -29,16 +30,20 @@ namespace eval ::fsat_bd {
         
         # Main processor interconnect GP0
         set axi_gp0 [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_cpu_interconnect]
-        set_property -dict [list CONFIG.NUM_MI {2} CONFIG.NUM_SI {1}] $axi_gp0
+        set_property -dict [list CONFIG.NUM_MI {3} CONFIG.NUM_SI {1}] $axi_gp0
 
         # IIC for sensors
         set axi_iic [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_iic:2.1 axi_iic_sens_0]
+
+        # IIC for open drain translator
+        set axi_iic_0 [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_iic:2.1 axi_iic_0]
         
         # SPI core for LTC2983
         set ltc_spi [create_bd_cell -type ip -vlnv xilinx.com:ip:axi_quad_spi:3.2 axi_quad_spi_0]
         set_property -dict [list \
             CONFIG.C_USE_STARTUP {0} \
             CONFIG.C_SPI_MODE {0} \
+            CONFIG.Master_mode {1} \
             CONFIG.C_NUM_SS_BITS {1} \
             CONFIG.C_NUM_TRANSFER_BITS {8} \
             CONFIG.C_FIFO_DEPTH {16} \
@@ -54,13 +59,18 @@ namespace eval ::fsat_bd {
         connect_bd_net [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins axi_cpu_interconnect/S00_ACLK]
         connect_bd_net [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins axi_cpu_interconnect/M00_ACLK]
         connect_bd_net [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins axi_cpu_interconnect/M01_ACLK]
+        connect_bd_net [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins axi_cpu_interconnect/M02_ACLK]
         connect_bd_net [get_bd_pins proc_sys_reset_0/interconnect_aresetn] [get_bd_pins axi_cpu_interconnect/ARESETN]
         connect_bd_net [get_bd_pins proc_sys_reset_0/peripheral_aresetn] [get_bd_pins axi_cpu_interconnect/S00_ARESETN]
         connect_bd_net [get_bd_pins proc_sys_reset_0/peripheral_aresetn] [get_bd_pins axi_cpu_interconnect/M00_ARESETN]
         connect_bd_net [get_bd_pins proc_sys_reset_0/peripheral_aresetn] [get_bd_pins axi_cpu_interconnect/M01_ARESETN]
+        connect_bd_net [get_bd_pins proc_sys_reset_0/peripheral_aresetn] [get_bd_pins axi_cpu_interconnect/M02_ARESETN]
 
         # LTC2983 SPI interface
-        make_bd_intf_pins_external -name ltc2983_spi [get_bd_intf_pins axi_quad_spi_0/SPI_0]
+        make_bd_pins_external -name ltc2983_spi_sck_o [get_bd_pins axi_quad_spi_0/sck_o]
+        make_bd_pins_external -name ltc2983_spi_io0_i [get_bd_pins axi_quad_spi_0/io0_o]
+        make_bd_pins_external -name ltc2983_spi_io1_o [get_bd_pins axi_quad_spi_0/io1_i]
+        make_bd_pins_external -name ltc2983_spi_ss_o [get_bd_pins axi_quad_spi_0/ss_o]
         connect_bd_intf_net [get_bd_intf_pins axi_quad_spi_0/AXI_LITE] [get_bd_intf_pins axi_cpu_interconnect/M01_AXI]
         connect_bd_net [get_bd_pins axi_quad_spi_0/s_axi_aclk] [get_bd_pins zynq_ps/FCLK_CLK0]
         connect_bd_net [get_bd_pins axi_quad_spi_0/ext_spi_clk] [get_bd_pins zynq_ps/FCLK_CLK0]
@@ -75,6 +85,14 @@ namespace eval ::fsat_bd {
         connect_bd_intf_net [get_bd_intf_pins iic_sens] [get_bd_intf_pins axi_iic_sens_0/IIC]
         connect_bd_net [get_bd_pins axi_iic_sens_0/iic2intc_irpt] [get_bd_pins irq_concat/In0]
         assign_bd_address -offset 0x41600000 -range 64K [get_bd_addr_spaces zynq_ps/Data] [get_bd_addr_segs axi_iic_sens_0/S_AXI/Reg]
+        
+        # I2C axi_iic for open drain translator
+        connect_bd_intf_net [get_bd_intf_pins axi_iic_0/S_AXI] [get_bd_intf_pins axi_cpu_interconnect/M02_AXI]
+        connect_bd_net [get_bd_pins axi_iic_0/s_axi_aclk] [get_bd_pins zynq_ps/FCLK_CLK0]
+        connect_bd_net [get_bd_pins axi_iic_0/s_axi_aresetn] [get_bd_pins proc_sys_reset_0/peripheral_aresetn]
+        connect_bd_intf_net [get_bd_intf_pins iic_0] [get_bd_intf_pins axi_iic_0/IIC]
+        connect_bd_net [get_bd_pins axi_iic_0/iic2intc_irpt] [get_bd_pins irq_concat/In2]
+        assign_bd_address -offset 0x41620000 -range 64K [get_bd_addr_spaces zynq_ps/Data] [get_bd_addr_segs axi_iic_0/S_AXI/Reg]
 
         # System Reset
         connect_bd_net -net zynq_ps_fclk_clk0 [get_bd_pins zynq_ps/FCLK_CLK0] [get_bd_pins proc_sys_reset_0/slowest_sync_clk] [get_bd_pins zynq_ps/M_AXI_GP0_ACLK]
@@ -82,7 +100,6 @@ namespace eval ::fsat_bd {
         connect_bd_net -net xlconstant_0_dout [get_bd_pins xlconstant_0/dout] [get_bd_pins zynq_ps/SDIO0_WP]
 
         # Fabric Interrupt
-        connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In2]
         connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In3]
         connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In4]
         connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In5]
--- hdl/xdc/flatsat2.xdc
+++ hdl/xdc/flatsat2.xdc
@@ -1,12 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
-# PS I2C Constraints
-set_property PACKAGE_PIN K18 [get_ports {iic_sens_sda_io}];
-set_property PACKAGE_PIN L17 [get_ports {iic_sens_scl_io}];
+# PL I2C Constraints
+set_property -dict {PACKAGE_PIN K18 IOSTANDARD LVCMOS18 PULLTYPE PULLUP} [get_ports {iic_sens_sda_io}];
+set_property -dict {PACKAGE_PIN L17 IOSTANDARD LVCMOS18 PULLTYPE PULLUP} [get_ports {iic_sens_scl_io}];
 
-set_property -dict {PACKAGE_PIN H17 IOSTANDARD LVCMOS18 PULLTYPE PULLUP} [get_ports ltc2983_spi_ss_io]; # SPI_ENB
-set_property -dict {PACKAGE_PIN J18 IOSTANDARD LVCMOS18} [get_ports ltc2983_spi_sck_io]; # SPI_CLK
-set_property -dict {PACKAGE_PIN H16 IOSTANDARD LVCMOS18} [get_ports ltc2983_spi_io0_io]; # SPI_MISO
-set_property -dict {PACKAGE_PIN H18 IOSTANDARD LVCMOS18} [get_ports ltc2983_spi_io1_io]; # SPI_MOSI
+# PS I2C Constraints
+set_property -dict {PACKAGE_PIN G19 IOSTANDARD LVCMOS18 PULLTYPE PULLUP} [get_ports {iic_0_sda_io}];
+set_property -dict {PACKAGE_PIN H20 IOSTANDARD LVCMOS18 PULLTYPE PULLUP} [get_ports {iic_0_scl_io}];
+
+set_property -dict {PACKAGE_PIN H17 IOSTANDARD LVCMOS18 PULLTYPE PULLUP} [get_ports ltc2983_spi_ss_o]; # SPI_ENB
+set_property -dict {PACKAGE_PIN J18 IOSTANDARD LVCMOS18} [get_ports ltc2983_spi_sck_o]; # SPI_CLK
+set_property -dict {PACKAGE_PIN H16 IOSTANDARD LVCMOS18} [get_ports ltc2983_spi_io0_i]; # SPI_MISO
+set_property -dict {PACKAGE_PIN H18 IOSTANDARD LVCMOS18} [get_ports ltc2983_spi_io1_o]; # SPI_MOSI
 
 set_property IOSTANDARD LVCMOS18 [get_ports -of_objects [get_iobanks 35]];
